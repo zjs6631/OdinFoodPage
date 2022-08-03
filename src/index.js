e49f54e23b9744051db58c34e674b892580d0a54
zjs6631 <zacharyjonessmith@gmail.com>
2022-08-03 12:47:12 -0400

    buttons now switch from home to menu page

--- src/index.js
+++ src/index.js
@@ -6,28 +6,41 @@ console.log("index.js connected");
 const container = document.getElementById('content')
 
 container.appendChild(homeContent());
-let homeDis = document.getElementById('homeID');
+
+
 let trackerVar = 0;
-const home = document.getElementById('homeBtn');
-const menu = document.getElementById('menuBtn');
 
-home.addEventListener('click', ()=>{
-    if(trackerVar != 0){
-        const currDisplay = document.getElementsByClassName('tabPage');
-        container.removeChild(currDisplay)
-        container.appendChild(homeContent());
-        trackerVar = 0;
+container.addEventListener('click', function(e){
+    if (e.target.classList.contains('barBtns')){
+        
+        const home = document.getElementById('homeBtn');
+        const menu = document.getElementById('menuBtn');
+        home.addEventListener('click', ()=>{
+            if(trackerVar != 0){
+                let currDisplay = document.getElementsByClassName('tabPage');
+                currDisplay[0].parentNode.removeChild(currDisplay[0]);
+                container.appendChild(homeContent());
+                trackerVar = 0;
+                console.log("Home button tracker variable worked!")
+            }
+            console.log("home button clicked!")
+        })
+        
+        
+        
+        
+        menu.addEventListener('click', ()=>{
+            if(trackerVar != 1){
+                let currDisplay = document.getElementsByClassName('tabPage');
+                currDisplay[0].parentNode.removeChild(currDisplay[0]);
+                container.appendChild(menuContent())
+                trackerVar = 1;
+                console.log("Menu button tracker variable worked!")
+            }
+            console.log("Menu button clicked!")
+        })
+
+
     }
 })
 
-
-
-
-menu.addEventListener('click', ()=>{
-    if(trackerVar != 1){
-        const currDisplay = document.getElementsByClassName('tabPage');
-        container.removeChild(container.currDisplay)
-        container.appendChild(menuContent())
-        trackerVar = 1;
-    }
-})
